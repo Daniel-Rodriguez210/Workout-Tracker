fcd7a6d68f89b2f1fe2718477b42216b28d3de25
Daniel Rodriguez <drod18@hotmail.com>
2020-11-12 09:39:37 -0600

    schema page setup taking in the seed info

--- userModel.js
+++ userModel.js
@@ -3,6 +3,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+    type: {
+        type: String,
+        trim: true,
+        required: "Exercise Type is Required"
+    },
 
-    
+    name: {
+
+        type: String,
+        trim: true,
+        required: "Name of Exercise is Required"
+    },
+
+    duration: {
+
+        type: Number,
+        trim: true,
+        required: "Duration of Exercise is Required"
+    },
+
+    distance: {
+        type: Number,
+    },
+
+    weight: {
+        type: Number,
+    },
+
+    reps: {
+        type: Number,
+    },
+
+    sets: {
+        type: Number,
+    },
+
 })
