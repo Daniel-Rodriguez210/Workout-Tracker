645a16cdb1e3f73d6c0e525271f8b3d12ac9e6ee
Daniel Rodriguez <drod18@hotmail.com>
2020-11-12 22:52:12 -0600

    created the routes and able to connect to port

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
 const mongojs = require("mongoose");
+const { Seeder } = require('mongo-seeding');
+
+
+
+
 
 const app = express();
 
@@ -8,6 +13,9 @@ app.use(logger("dev"));
 
 app.use(express.static('public'));
 app.use(express.static('seeders'));
+
+
+require("./Develop/routes/html-routes")(app);
 
 
 
@@ -18,4 +26,4 @@ app.use(express.static('seeders'));
 
 app.listen(3000, () => {
     console.log("App running on port 3000!");
-  });
+});
