0edfad6f9115baf669667ecfb12a9fea142e4609
Daniel Rodriguez <drod18@hotmail.com>
2020-11-17 17:37:36 -0600

    fix server page connect to db and link to routes

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const logger = require("morgan");
-const mongojs = require("mongoose");
+const mongoose = require("mongoose");
 const { Seeder } = require('mongo-seeding');
 
-
-
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbWorkout", { useNewUrlParser: true });
 
 const app = express();
 
 app.use(logger("dev"));
-
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 app.use(express.static('public'));
 app.use(express.static('seeders'));
 
-
-require("./Develop/routes/html-routes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 
 
